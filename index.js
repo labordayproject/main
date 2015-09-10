e30940a57a6adaae95c45cbe048a8920b7d93b60
Adam Taitano <adam.taitano@gmail.com>
2015-09-09 17:14:23 -0700

    nothing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
-// var app = express();
-//
+var app = express();
+
 // app.set('port', (process.env.PORT || 5000));
 //
 // app.use(express.static(__dirname + '/public'));
@@ -8,11 +8,11 @@ var express = require('express');
 // // views is directory for all template files
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
-//
+
 // app.get('/', function(request, response) {
 //   response.render('pages/index');
 // });
-//
+
 // app.listen(app.get('port'), function() {
 //   console.log('Node app is running on port', app.get('port'));
 // });
